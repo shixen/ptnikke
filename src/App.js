699d64b886fc96b7b508fbfed38559347b0696c7
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-08-14 18:45:05 +0000

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import InfoBox from './components/InfoBox';
+import NavBar from './components/NavBar';
 
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
+      <InfoBox/>
     </div>
   );
 }
