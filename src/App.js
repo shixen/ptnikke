132d8e92b6995796655b2eea387039ec4591d14c
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-08-16 15:42:46 +0000

    add new apps tp app.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
+import Posts from './components/ApiPosts';
+import ApiWorkout from './components/ApiWorkout';
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Posts/>
+      <ApiWorkout/>
       <Container className={styles.Main}>
         <Switch>
           <Route exact path="/" render={() => <h1>Home page</h1>} />
