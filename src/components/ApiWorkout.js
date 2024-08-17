30862d6f2697cfa646feb37fa5ad2af5c734a9cf
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-08-17 11:26:56 +0000

    split json data to new line for workouts

--- src/components/ApiWorkout.js
+++ src/components/ApiWorkout.js
@@ -15,15 +15,25 @@ const ApiWorkout = () => {
       });
   }, []);
 
+  const formatExercises = (exercises) => {
+    return exercises.split('\n').filter(item => item.trim() !== '');
+  };
+
   return (
     <Container>
-      <h1 className="my-4">Workout!</h1>
+      <h1 className="my-4">Workout Plan</h1>
       <ListGroup>
         {posts.map(post => (
           <ListGroup.Item key={post.id}>
             <h5>{post.title}</h5>
             <p><strong>Description:</strong> {post.description}</p>
-            <p><strong>reps / sets:</strong> {post.exercises}</p>
+            <ListGroup>
+              {formatExercises(post.exercises).map((exercise, index) => (
+                <ListGroup.Item key={index}>
+                  {exercise}
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
           </ListGroup.Item>
         ))}
       </ListGroup>
