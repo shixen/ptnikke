e97d5d40fff41bb7b5eee252701a2f2751bf5177
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-08-17 16:09:08 +0000

    change navling paths

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,45 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import styles from '../styles/NavBar.module.css';
 import { NavLink } from 'react-router-dom';
+import styles from '../styles/NavBar.module.css';
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} bg="light" expand="md" fixed="top">
       <Container>
-        <NavLink to="/" exact activeClassName="active" className="navbar-brand">
-          PT Niklas
+        <NavLink 
+          to="/" 
+          className={({ isActive }) => isActive ? "navbar-brand active" : "navbar-brand"}
+        >
+          N.E Workout
         </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <NavLink to="/" exact activeClassName="active" className="nav-link">Home</NavLink>
-            <NavLink to="/about" exact activeClassName="active" className="nav-link">About me</NavLink>
-            <NavLink to="/signup" exact activeClassName="active" className="nav-link">Sign up</NavLink>
+            <NavLink 
+              to="/" 
+              className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
+            >
+              Home
+            </NavLink>
+            <NavLink 
+              to="/about" 
+              className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
+            >
+              About me
+            </NavLink>
+            <NavLink 
+              to="/signup" 
+              className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
+            >
+              Sign up
+            </NavLink>
+            <NavLink 
+              to="/login" 
+              className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
+            >
+              Login/logout
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
