a2553cbf37f459ad55b2f8be73c8c97ecb487ec7
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-08-17 16:10:18 +0000

    change route path in app.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import NavBar from './components/NavBar';
 import ApiPosts from './components/ApiPosts';
 import ApiWorkout from './components/ApiWorkout';
 import Signup from './components/SignUp';
-import './App.module.css';
 import AboutMe from './components/AboutMe';
+import './App.module.css';
+import AuthPage from './components/LoginLogout';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <NavBar />
-        <Container className="mt-5">
-          <Switch>
-            <Route exact path="/" render={() => (
-              <div>
-                <ApiPosts />
-                <ApiWorkout />
-              </div>
-            )} />
-            <Route exact path="/signup" component={Signup} />
-            <Route exact path="/about" component={AboutMe} />
-            <Route render={() => <p>Page not found!</p>} />
-          </Switch>
-        </Container>
-      </div>
-    </Router>
+    <div>
+      <NavBar />
+      <Container className="mt-5">
+        <Routes>
+          <Route path="/" element={<><ApiPosts /><ApiWorkout /></>} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/about" element={<AboutMe />} />
+          <Route path="/login" element={<AuthPage />} />
+          <Route path="*" element={<p>Page not found!</p>} />
+        </Routes>
+      </Container>
+    </div>
   );
 }
 
