cc3c810b740852ffa24e34dba470d08e38452622
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-08-17 10:46:05 +0000

    fixed routers and add apps

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import styles from "./App.module.css";
-import NavBar from "./components/NavBar";
-import Container from "react-bootstrap/Container";
-import { Route, Switch } from "react-router-dom";
-import Posts from './components/ApiPosts';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import NavBar from './components/NavBar';
+import ApiPosts from './components/ApiPosts';
 import ApiWorkout from './components/ApiWorkout';
+import Signup from './components/SignUp';
+import './App.module.css';
+import AboutMe from './components/AboutMe';
 
 function App() {
   return (
-    <div className={styles.App}>
-      <NavBar />
-      <Posts/>
-      <ApiWorkout/>
-      <Container className={styles.Main}>
-        <Switch>
-          <Route exact path="/" render={() => <h1>Home page</h1>} />
-          <Route exact path="/about" render={() => <h1>Sign up</h1>} />
-          <Route render={() => <p>Page not found!</p>} />
-        </Switch>
-      </Container>
-    </div>
+    <Router>
+      <div>
+        <NavBar />
+        <Container className="mt-5">
+          <Switch>
+            <Route exact path="/" render={() => (
+              <div>
+                <ApiPosts />
+                <ApiWorkout />
+              </div>
+            )} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/about" component={AboutMe} />
+            <Route render={() => <p>Page not found!</p>} />
+          </Switch>
+        </Container>
+      </div>
+    </Router>
   );
 }
 
