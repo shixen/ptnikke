a141bba6f3d77a83d10863b4d52dc991f4f30c15
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-08-26 20:11:38 +0000

    add apps to app.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import ApiPosts from './components/ApiPosts';
 import ApiWorkout from './components/ApiWorkout';
 import Signup from './components/SignUp';
 import AboutMe from './components/AboutMe';
-import AuthPage from './components/LoginLogout';
+import Login from './components/login';
+import Logout from './components/logout';
 
 function App() {
   return (
@@ -17,7 +18,8 @@ function App() {
           <Route path="/" element={<><ApiPosts /><ApiWorkout /></>} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/about" element={<AboutMe />} />
-          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Logout />} />
           <Route path="*" element={<p>Page not found!</p>} />
         </Routes>
       </Container>
